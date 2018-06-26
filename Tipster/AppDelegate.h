53cbc330b86d01a9b1e68682db6bc992e38b0800
Sarah Embry <skembry@fb.com>
2018-06-26 13:20:54 -0700

    Initial Commit

---
+++ Tipster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tipster
+//
+//  Created by Sarah Embry on 6/26/18.
+//  Copyright © 2018 skembry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
